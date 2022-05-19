ce73fa96f9638791c823e4a778e075764270f730
Oussama Elkhaldaoui <93930380+elkhaldaoui@users.noreply.github.com>
2022-05-19 14:14:38 +0100

    fixed login href

--- views/index.view.php
+++ views/index.view.php
@@ -23,7 +23,7 @@ background-repeat: no-repeat; background-size: 100%; background-position: center
         <div class="text-center text-black" style="margin-top: 200px;">
             <h1><b>Hello!</b></h1>
             <h3>No Account ? ask your admin to create one for you </h3>
-            <h5>Already have an account ? <a href="login.html">Login here.</a></h5>
+            <h5>Already have an account ? <a href="./login">Login here.</a></h5>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
